e95bc4a8ecec5070f1da9137318431bfa590e5f4
Ryan Hartman <ryandavidhartman@gmail.com>
2023-01-25 21:55:53 -0500

    getting into the swings of things, SWING get it?

--- udemy1/06-swingtest/src/com/example/SwingTest.java
+++ udemy1/06-swingtest/src/com/example/SwingTest.java
@@ -1,12 +1,58 @@
 package com.example;
 
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 class FrameWindow extends JFrame {
 	
+	JLabel labelMessage;
+	JTextField textField;
+	JButton button;
+	JPanel p1,p2,p3;
+	
 	public FrameWindow(String title) {
 		super(title);
-		setSize(640, 480);
+		setSize(320, 240);
+		setLayout(new GridLayout(3, 1, 5, 5));
+		
+		labelMessage = new JLabel("Hello World");
+		textField = new JTextField(20);
+		button = new JButton("Click me");
+		
+		p1 = new JPanel();
+		p2 = new JPanel();
+		p3 = new JPanel();
+		
+		p1.add(labelMessage);
+		p2.add(textField);
+		p3.add(button);
+		
+		add(p1);
+		add(p2);
+		add(p3);
+		
+		button.addActionListener(new ButtonHandler());
+	}
+	
+	class ButtonHandler implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == button) {
+				JOptionPane.showMessageDialog(null,"Hello " + textField.getText());
+				labelMessage.setText("Hello " + textField.getText());
+			}
+			
+		}
+		
 	}
 }
 
