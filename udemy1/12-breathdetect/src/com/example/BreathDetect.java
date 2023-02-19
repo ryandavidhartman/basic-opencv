70778fa04fb8e1b20ed058182eb62f4d7c3e262f
Ryan Hartman <ryandavidhartman@gmail.com>
2023-02-19 02:49:36 -0500

    going back to open cv 2.4.x

--- udemy1/12-breathdetect/src/com/example/BreathDetect.java
+++ udemy1/12-breathdetect/src/com/example/BreathDetect.java
@@ -1,10 +1,35 @@
 package com.example;
 
-public class BreathDetect {
+import java.util.LinkedList;
+import java.util.Queue;
 
+import org.opencv.core.Core;
+
+import processing.core.PApplet;
+
+public class BreathDetect extends PApplet {
+	
+	public Queue<Double> periods;
+	public static int currentDirection = 0;
+	public static boolean toShutdown = false;
+	public static double currentPointY = 300.0f;
+	
+	public void setup() {
+		
+		periods = new LinkedList<>();
+		initQueue();
+	}
+	
+	private void initQueue() {
+		for(int i = 0; i<5; i++) {
+			periods.add(8.0);
+		}
+	}
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		PApplet.main(BreathDetect.class.getName());
 	}
 
 }
